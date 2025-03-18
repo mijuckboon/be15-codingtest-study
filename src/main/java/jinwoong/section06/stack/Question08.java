85e4a48bb0f5f5e8d99367109f3edd3564f11ecd
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-03-18 23:27:08 +0900

    style: Question08 코드 변경
    - 스택 이용 방법 주석 해제
    - return 문 수정
    - 메서드 이름 및 순서 변경 (정석적인 방법이 위로 오게)

--- src/main/java/jinwoong/section06/stack/Question08.java
+++ src/main/java/jinwoong/section06/stack/Question08.java
@@ -1,8 +1,31 @@
 package jinwoong.section06.stack;
 
+import java.util.Stack;
+
 public class Question08 {
 
     boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push('(');
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+
+        }
+        return stack.isEmpty();
+//        if (stack.isEmpty()) return true;
+//
+//        return false;
+    }
+
+    boolean solution2(String s) {
         // 카운터 변수: 효율은 좋으나 정합성 떨어짐
         int count = 0;
 
@@ -19,24 +42,5 @@ public class Question08 {
         return (count == 0);
     }
 
-//    boolean solution(String s) {
-//
-//        Stack<Character> stack = new Stack<>();
-//        for (int i = 0; i < s.length(); i++) {
-//            if (s.charAt(i) == '(') {
-//                stack.push('(');
-//            } else {
-//                if (stack.isEmpty()) {
-//                    return false;
-//                } else {
-//                    stack.pop();
-//                }
-//            }
-//
-//        }
-//        if (stack.isEmpty()) return true;
-//
-//        return false;
-//    }
 
 }
