413058cf10a152cb91e3c24c2372b392d89933e2
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-03-18 23:20:09 +0900

    feat: Question09 코드 추가
    - Question09.java 추가

---
+++ src/main/java/jinwoong/section06/stack/Question09.java
@@ -0,0 +1,31 @@
+package jinwoong.section06.stack;
+
+import java.util.Stack;
+
+/**
+ * 10진수를 입력받아 2진수로 변환해 반환하는 solution( ) 함수를 구현하세요.
+ * <p>
+ * 제약조건 <br/>
+ * decimal은 1이상 10억 미만의 자연수
+ */
+public class Question09 {
+    private static String solution(int num) {
+        Stack <Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            int rem = num % 2;
+            num /= 2;
+            stack.push(rem);
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+        System.out.println(solution(27));
+        System.out.println(solution(12345));
+    }
+}
