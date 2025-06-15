d999de075184ebc6c6a27566a5f4449beebd2cb2
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-15 15:40:24 +0900

    refactor: 문제 50 주석 추가 (따른 풀이)

--- src/main/java/jinwoong/section13/sort/section12/backtracking/Question50.java
+++ src/main/java/jinwoong/section13/sort/section12/backtracking/Question50.java
@@ -3,6 +3,7 @@ package jinwoong.section13.sort.section12.backtracking;
 public class Question50 {
     /**
      * 계수 정렬 구현하기
+     *
      * @param s 알파벳 소문자로 이루어진 문자열 (길이 1 이상 10,000 이하)
      * @return 정렬된 문자열
      */
@@ -13,10 +14,14 @@ public class Question50 {
         for (char ch : chars) {
             counts[ch - 'a'] += 1;
         }
+//        for (int i = 0; i < s.length(); i++) { -> 성능 효율적
+//            counts[s.charAt(i) - 'a']++;
+//        }
 
         StringBuilder sb = new StringBuilder();
         for (char ch = 'a'; ch <= 'z'; ch++) {
-             sb.append(String.valueOf(ch).repeat(Math.max(0, counts[ch - 'a'])));
+            sb.append(String.valueOf(ch).repeat(Math.max(0, counts[ch - 'a']))); // Character.toString()
+//            sb.append((ch + "").repeat(counts[ch - 'a'])); // 0 이상만 들어가므로 Math.max는 딱히 필요 X (음수일 때 대비한 안전장치)
         }
 
         return sb.toString();
