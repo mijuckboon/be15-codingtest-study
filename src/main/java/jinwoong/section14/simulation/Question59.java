71a2e734e111839e0827741232184c34ee0d0b5f
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-22 21:45:39 +0900

    feat: 문제 59 1차 시도

---
+++ src/main/java/jinwoong/section14/simulation/Question59.java
@@ -0,0 +1,40 @@
+package jinwoong.section14.simulation;
+
+import java.util.Arrays;
+
+public class Question59 {
+    /**
+     * 배열 회전하기
+     * <p>
+     * 제약조건 <br/>
+     * n은 1 이상 4 이하의 자연수 <br/>
+     * 2차원 배열의 행과 열의 크기는 같고, 행의 크기는 10을 넘지 않음
+     * </p>
+     *
+     * @param arr 2차원 배열
+     * @param n 시계방향 회전 횟수
+     * @return 회전 결과
+     */
+    int[][] solution(int[][] arr, int n) {
+        int len = arr.length;
+
+        int[][] prev = Arrays.copyOf(arr, len);
+        for (int row = 0; row < len; row++) {
+            for (int col = 0; col < len; col++) {
+                prev[row][col] = arr[row][col];
+            }
+        }
+
+        int[][] result = new int[len][len];
+
+        for (int rep = 0; rep < n; rep++) {
+            for (int row = 0; row < len; row++) {
+                for (int col = 0; col < len; col++) {
+                    result[col][len - 1 - row] = prev[row][col];
+                }
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/jinwoong/section14/simulation/Question59Test.java
@@ -0,0 +1,66 @@
+package jinwoong.section14.simulation;
+
+import jinwoong.section13.sort.Question50;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Question59Test {
+    Question59 question;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question59();
+    }
+
+    static Stream<Arguments> provideTestCases() {
+        return Stream.of(
+                Arguments.arguments(
+                        new int[][] {
+                                {1, 2, 3, 4},
+                                {5, 6, 7, 8},
+                                {9, 10, 11, 12},
+                                {13, 14, 15, 16}
+                        },
+                        1,
+                        new int[][] {
+                                {13, 9, 5, 1},
+                                {14, 10, 6, 2},
+                                {15, 11, 7, 3},
+                                {16, 12, 8, 4},
+                        }
+                ),
+                Arguments.arguments(
+                        new int[][] {
+                                {1, 2, 3, 4},
+                                {5, 6, 7, 8},
+                                {9, 10, 11, 12},
+                                {13, 14, 15, 16}
+                        },
+                        2,
+                        new int[][] {
+                                {16, 15, 14, 13},
+                                {12, 11, 10, 9},
+                                {8, 7, 6, 5},
+                                {4, 3, 2, 1},
+                        }
+                )
+        );
+    }
+
+    @DisplayName("Question59 테스트")
+    @ParameterizedTest(name = "test {index}: arr={0}, n={1}")
+    @MethodSource("provideTestCases")
+    void testSolution(int[][] arr, int n, int[][] expected) {
+        int[][] result = question.solution(arr, n);
+        Assertions.assertArrayEquals(expected, result);
+    }
+
+}
