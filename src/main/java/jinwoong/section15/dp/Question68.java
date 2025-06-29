6028122d614d3df9473c078bd7126570a3ea9605
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-06-29 23:35:20 +0900

    feat: 문제 68 10차 시도 (성공), 방법2 1차 시도

--- src/main/java/jinwoong/section15/dp/Question68.java
+++ src/main/java/jinwoong/section15/dp/Question68.java
@@ -11,7 +11,7 @@ public class Question68 {
      * @param nums 최대 길이 1,000의 정수 배열
      * @return LIS의 길이
      */
-    int solution(int[] nums) {
+    int solution2(int[] nums) {
         int[] lis = new int[nums.length + 1];
         Arrays.fill(lis, 1);
 
@@ -19,6 +19,23 @@ public class Question68 {
             for (int j = 1; j <= i; j++) {
                 if (nums[i - 1] > nums[j - 1]) {
                     lis[i] = Math.max(lis[i], lis[j] + 1);
+                }
+            }
+
+        }
+
+        return Arrays.stream(lis).max().getAsInt();
+    }
+
+    int solution(int[] nums) {
+        int[] lis = new int[nums.length + 1];
+        lis[1] = 1;
+
+        for (int i = 1; i < lis.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i - 1]) {
+                    lis[i] = lis[j] + 1;
+                    break;
                 }
             }
 
